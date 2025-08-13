811fd762b0246d9ca0ddf4df23a24cd284a67810
Delta student <mdminhaj005chakia@gmail.com>
2025-08-13 17:25:47 +0530

    initial commit

---
+++ src/WeatherApp.jsx
@@ -0,0 +1,28 @@
+import SearchBox from './SearchBox';
+import InfoBox from './InfoBox';
+import { useState } from 'react';
+
+
+export default function WeatherApp() {
+    const [weatherInfo, setWeatherInfo] = useState({
+        city: "Delhi",
+        feelslike: 42.4,
+        humidity: 54,
+        temp: 35.4,
+        tempMax: 35.4,
+        tempMin: 35.4,
+        weather: "overcast clouds",
+    });
+
+    let updateInfo = (newInfo) => {
+        setWeatherInfo(newInfo);
+    }
+
+    return(
+        <div style={{textAlign:"center"}}>
+            <h2>Weather App by Delta</h2>
+            <SearchBox updateInfo={updateInfo}/>
+            <InfoBox info={weatherInfo} />
+        </div>
+    );
+}
